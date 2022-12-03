3f5c53e41b8a149934d44dbfbd300f40ecefceb5
powcoder <powcoder@163.com>
2022-12-03 19:48:14 +0800

    COMP9318 Lab2 2018

---
+++ README.md
@@ -0,0 +1,9 @@
+# COMP9318 Lab2 2018
+# 加微信 powcoder
+
+# QQ 1823890830
+
+# Programming Help Add Wechat powcoder
+
+# Email: powcoder@163.com
+
---
+++ submission.py
@@ -0,0 +1,49 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+## import modules here 
+import pandas as pd
+import numpy as np
+
+
+################# Question 1 #################
+
+# helper functions
+def project_data(df, d):
+    # Return only the d-th column of INPUT
+    return df.iloc[:, d]
+
+def select_data(df, d, val):
+    # SELECT * FROM INPUT WHERE input.d = val
+    col_name = df.columns[d]
+    return df[df[col_name] == val]
+
+def remove_first_dim(df):
+    # Remove the first dim of the input
+    return df.iloc[:, 1:]
+
+def slice_data_dim0(df, v):
+    # syntactic sugar to get R_{ALL} in a less verbose way
+    df_temp = select_data(df, 0, v)
+    return remove_first_dim(df_temp)
+
+def buc_rec_optimized(df):# do not change the heading of the function
+    pass # **replace** this line with your code    
+
+################# Question 2 #################
+
+def v_opt_dp(x, num_bins):# do not change the heading of the function
+    pass # **replace** this line with your code
